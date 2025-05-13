8bba9cfa9c16faadd242d964cdb100529fca5f91
sgarvey1 <147288594+sgarvey1@users.noreply.github.com>
2025-05-13 09:30:02 -0400

    Created logic for main file

--- Main.py
+++ Main.py
@@ -1,6 +1,22 @@
 import ClassManager
-#ClassManager.createClass()
-#ClassManager.addStudent()
-#ClassManager.classCheckin()
-#ClassManager.DisplayRoster("Engineering 1")
-ClassManager.deleteName("Engineering 2")
+while True:
+    print("What would you like to do?")
+    print("Add Class (AD) | Add Student to Class (AS) | Delete Student from Class (DS) | Check-in Class (CC) | Display Class (DC)")
+    function = input("Type in letters for one of the above functions:")
+    if function == "AD":
+        ClassManager.createClass()
+    if function == "AS":
+        ClassManager.addStudent()
+    if function == "DS":
+        ClassManager.deleteName(input("Which class would you like to delete a student from?"))
+    if function == "CC":
+        ClassManager.classCheckin()
+    if function == "DC":
+        print("Would you like to display class attendence(A) or class roster(R)?")
+        DisplayFile = input("Type the letter of the option you choose:")
+        if DisplayFile == "A":
+            ClassManager.DisplayAttendence(input("Which class do you want to view?"))
+        if DisplayFile == "R":
+            ClassManager.DisplayRoster(input("Which class do you want to view?"))
+        else:
+            print("Invalid Input")
