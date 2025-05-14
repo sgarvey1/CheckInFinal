125386aca3a061d9b8fbce3f8d4cba382ec9d876
sgarvey1 <147288594+sgarvey1@users.noreply.github.com>
2025-05-13 21:10:53 -0400

    Changed name of file from ClassManager to ClassManagerFunctions

--- Main.py
+++ Main.py
@@ -1,4 +1,4 @@
-import ClassManager
+import ClassManagerFunctions
 import os
 
 while True:
@@ -6,22 +6,22 @@ while True:
     print("Add Class (AD) | Remove Class (RC) | Add Student to Class (AS) | Delete Student from Class (DS) | Check-in Class (CC) | Display Class (DC)")
     function = input("Type in letters for one of the above functions: ")
     if function == "AD":
-        ClassManager.createClass()
+        ClassManagerFunctions.createClass()
     if function == "RC":
-        ClassManager.removeClass()
+        ClassManagerFunctions.removeClass()
     if function == "AS":
-        ClassManager.addStudent()
+        ClassManagerFunctions.addStudent()
     if function == "DS":
-        ClassManager.deleteName(input("Type name of class you wish to delete student from: "))
+        ClassManagerFunctions.deleteName(input("Type name of class you wish to delete student from: "))
     if function == "CC":
-        ClassManager.classCheckin()
+        ClassManagerFunctions.classCheckin()
     if function == "DC":
         print("Would you like to display class attendence(A) or class roster(R)?")
         DisplayFile = input("Type the letter of the option you choose: ")
         if DisplayFile == "A":
-            ClassManager.DisplayAttendence(input("Type name of class you wish to view: "))
+            ClassManagerFunctions.DisplayAttendence(input("Type name of class you wish to view: "))
         if DisplayFile == "R":
-            ClassManager.DisplayRoster(input("Type name of class you wish to view: "))
+            ClassManagerFunctions.DisplayRoster(input("Type name of class you wish to view: "))
         else:
             print("Invalid Input")
     else:
